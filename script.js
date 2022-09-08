2c7128874d687402c592891ac87b735bbd00ba5d
Calvin Roberts <roberts.calvin85@gmail.com>
2022-09-08 23:36:50 +0100

    Create function for ids

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+var emailField = document.getElementById('email-field'),
+  emailLabel = document.getElementById('email-label'),
+  emailError = document.getElementById('email-error');
+
+function validateEmail() {
+  emailLabel.style.bottom = '45px';
+}
