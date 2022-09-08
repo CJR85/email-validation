253d60529b4169c1677cbd2276b2f143c892ab4d
Calvin Roberts <roberts.calvin85@gmail.com>
2022-09-08 23:52:19 +0100

    Add if statement to valiate email function

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ var emailField = document.getElementById('email-field'),
 
 function validateEmail() {
   emailLabel.style.bottom = '45px';
+
+  if (
+    !emailField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)
+  ) {
+    emailError.innerHTML = 'Please enter a valid email';
+    return false;
+  }
+
+  emailError.innerHTML = '';
+  return true;
 }
