c77810bc46dd124c799b93b08ff8364c60fcc555
Calvin Roberts <roberts.calvin85@gmail.com>
2022-09-08 23:57:34 +0100

    Style bottom border color for valid and invalid email

--- script.js
+++ script.js
@@ -10,9 +10,13 @@ function validateEmail() {
     !emailField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)
   ) {
     emailError.innerHTML = 'Please enter a valid email';
+    emailField.style.borderBottomColor = 'red';
+    emailError.style.top = '120%';
     return false;
   }
 
   emailError.innerHTML = '';
+  emailField.style.borderBottomColor = 'green';
+  emailError.style.top = '100%';
   return true;
 }
